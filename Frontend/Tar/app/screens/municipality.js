d0d71e00d207fc8bc0055278cdf03f7016978b88
DirkWilkeHoffmann <wilkehoffmann@gmail.com>
2024-08-14 14:48:29 +0200

    front end working

--- Frontend/Tar/app/screens/municipality.js
+++ Frontend/Tar/app/screens/municipality.js
@@ -5,6 +5,8 @@ import {
   FlatList,
   StyleSheet,
   ActivityIndicator,
+  Linking,
+  TouchableOpacity,
 } from "react-native";
 import axios from "axios"; // You'll need to install this package
 import { db } from "../../FirebaseConfig";
@@ -24,9 +26,12 @@ const MunicipalityScreen = () => {
       // const response = await axios.get(
       //   "https://your-firebase-api-endpoint.com/potholes"
       // );
-      const potholeCollection = collection(db, 'potholes');
+      const potholeCollection = collection(db, "potholes");
       const potholeSnapshot = await getDocs(potholeCollection);
-      const potholeList = potholeSnapshot.docs.map((doc) => ({...doc.data() , id : doc.id}));
+      const potholeList = potholeSnapshot.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
       setPotholes(potholeList);
       setLoading(false);
     } catch (error) {
@@ -35,18 +40,25 @@ const MunicipalityScreen = () => {
     }
   };
 
-  const renderPotholeItem = ({ item }) => (
+  const renderPotholeItem = ({ item }) => {
+    const googleMapsUrl = `https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(
+      item.location
+    )}`;
 
-    console.log(item.reported_date),
-    <View style={styles.potholeItem}>
-      <Text style={styles.potholeTitle}>Pothole ID: {item.id}</Text>
-      <Text>Location: {item.location}</Text>
-      <Text>
-        Reported on: {(item.reported_date.toDate()).toLocaleDateString()}
-      </Text>
-      <Text>Status: {item.status}</Text>
-    </View>
-  );
+    return (
+      <View style={styles.potholeItem}>
+        <Text style={styles.potholeTitle}>Pothole ID: {item.id}</Text>
+        <Text>Location: {item.location}</Text>
+        <Text>
+          Reported on: {item.reported_date.toDate().toLocaleDateString()}
+        </Text>
+        <Text>Status: {item.status}</Text>
+        <TouchableOpacity onPress={() => Linking.openURL(googleMapsUrl)}>
+          <Text style={styles.linkText}>View in Google Maps</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  };
 
   if (loading) {
     return (
@@ -104,6 +116,12 @@ const styles = StyleSheet.create({
     textAlign: "center",
     marginTop: 50,
     fontSize: 18,
+  },
+  linkText: {
+    color: "#1E90FF", // Blue color for the link
+    marginTop: 8,
+    fontSize: 16,
+    textDecorationLine: "underline", // Underline the text to signify it's a link
   },
 });
 
